d0d81bba4fd0e52f8c550a21b050686abafe7b1f
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-10-24 19:58:23 +0300

    Add dependencies & configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["tsconfig.json", "tsconfig.app.json", "tsconfig.node.json"],
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-eslint": eslintPluginReact,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: eslintPluginPrettier,
     },
     rules: {
+      ...eslintConfigPrettier.rules,
+      ...eslintPluginPrettier.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "prefer-const": "error",
     },
-  },
-)
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+import { resolve } from 'node:path';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react-phonebook/' : '/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      components: resolve(__dirname, 'src/components'),
+      constants: resolve(__dirname, 'src/constants'),
+    },
+  },
+});
